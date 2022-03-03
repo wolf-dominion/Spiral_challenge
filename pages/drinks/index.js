7f9d6686e970aec00db9546f6fda5813cd7af841
wolf-dominion <hannahdruryutd@gmail.com>
2022-03-02 20:43:39 -0700

    styling and icons for results complete

--- pages/drinks/index.js
+++ pages/drinks/index.js
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import useSWR from "swr";
 import Link from 'next/link'
 import styles from '../../styles/drinks.module.css'
+import Input from '@mui/material/Input'
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search'
+import CancelIcon from '@mui/icons-material/Cancel';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
@@ -29,10 +34,47 @@ export default function Example() {
 
   }, [drinkQuery, drink]);
 
+  const displayEmptyRows = (rows) => {
+
+    const emptyRows = []
+
+    for(let i = 0; i < rows; i++){
+        emptyRows.push(<div className={styles.gridItem}></div>)
+    }
+
+      return emptyRows.map(row => {
+        return (
+          <div className={styles.gridItem} key={row.indexOf+'empty-row'}>
+          </div>
+        )
+    })
+
+  }
+
   return (
     <div className={styles.gridContainer}>
       <div className={styles.title} >Thirsty</div>
-      <input className={styles.searchbar} onChange={handleChange} value={drinkQuery} placeholder='Find a drink'/>
+      {/* <input type='search' className={styles.searchbar} onChange={handleChange} value={drinkQuery} placeholder='Find a drink'/> */}
+      
+      
+      <Input
+        disableUnderline={true}
+        className={styles.searchbar} 
+        onChange={handleChange} 
+        value={drinkQuery} 
+        placeholder='Find a drink'
+        startAdornment={
+          <InputAdornment position='start'>
+            <SearchIcon />
+          </InputAdornment>
+        }
+        endAdornment={
+          <InputAdornment position='start'>
+            <CancelIcon sx={{ color: "#8e8e93" }}/>
+          </InputAdornment>
+        }
+      />
+      <span className={styles.divider}></span>
       { error && <div>failed to load</div> }
       {/* {!data && <div>loading...</div> } */}
       {/* { data && <pre>{JSON.stringify(data, null, 2)}</pre>} */}
@@ -41,7 +83,7 @@ export default function Example() {
             data.drinks.map(drink => {
               return (
                 <div className={styles.gridItem} key={drink.strDrink}>
-                  <img className={styles.thumbnail} src={drink.strDrinkThumb} width='30' height='30' alt='drink'/>
+                  <img className={styles.thumbnail} src={drink.strDrinkThumb} alt='drink'/>
                   <div className={styles.drinkname}><Link
                     href={{
                       pathname: `drinks/${drink.strDrink}`,
@@ -52,70 +94,15 @@ export default function Example() {
                     <span>{drink.strDrink}</span>
                   </Link>
                   </div>
+                  <ArrowForwardIosIcon sx={{ color: "#b9b9b9" }} className={styles.arrow}/>
                 </div>
               )
           })
+        }
+        {
+          data && data.drinks && data.drinks.length < 12 ? displayEmptyRows(12 - data.drinks.length) : null
         }
       </div>
     </div>
   )
 }
-
-// import Link from 'next/link'
-// import { useState } from 'react'
-// import useSWR from 'swr'
-// import { styles } from 'recipe.module.css';
-
-//   const fetcher = async (drink) => {
-//     const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-//     const data = await res.json()
-//     console.log('fetcher triggered', drink)
-//     console.log('data:', data)
-//     return data.drinks
-//   }
-
-// function Drinks({ drinks }) {
-
-//   const [drinkQuery, setdrinkQuery] = useState()
-//   const [drink, setDrink] = useState(undefined);
-
-//   const handleChange = (e) => {
-//     console.log('changed')
-//     setdrinkQuery(e.target.value);
-//   };
-
-//     const { data, error } = useSWR('drinks', fetcher(drinkQuery))
-
-//     return (
-//       <>
-//         <h1>Thirsty</h1>
-//         <input onChange={handleChange} value={drinkQuery} placeholder='Find a drink'/>
-//         <br />
-
-//         {/* {
-//           data.drinks.map(drink => {
-//             return (
-//               <div key={drink.strDrink}>
-//                 <Link href={`drinks/${drink.strDrink}`} passHref>
-//                   <h2>{drink.strDrink}</h2>
-//                 </Link>
-//               </div>
-//             )
-//           })
-//         } */}
-//       </>
-//     )
-//   }
-  
-//   export default Drinks
-
-// // export async function getStaticProps(){
-// //   const res = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-// //   const data = await res.json()
-
-// //   return {
-// //     props: {
-// //       drinks: data.drinks
-// //     }
-// //   }
-// // }
